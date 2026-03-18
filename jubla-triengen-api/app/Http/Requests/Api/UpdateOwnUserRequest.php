aebfb4206f32ff5b0cd488e56a16bb7259455d39
Nils Utiger <nils.utiger@suva.ch>
2026-03-18 12:54:07 +0100

    feat; implement simple auth functionality

---
+++ jubla-triengen-api/app/Http/Requests/Api/UpdateOwnUserRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
+
+class UpdateOwnUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user() !== null;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['sometimes', 'string', 'max:255'],
+            'email' => [
+                'sometimes',
+                'email',
+                'max:255',
+                Rule::unique('users', 'email')->ignore($this->user()?->id),
+            ],
+            'current_password' => ['required_with:password', 'current_password'],
+            'password' => ['sometimes', 'confirmed', Password::defaults()],
+        ];
+    }
+}
